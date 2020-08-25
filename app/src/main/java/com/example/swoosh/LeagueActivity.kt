16c3cc848734f439bbf9a2a5ff91f533a7053fc5
rajeshsahu09 <rajeshsahuise@gmail.com>
2020-08-25 19:26:39 +0530

    learned passing variables to other activity using intent

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class LeagueActivity : BaseActivity() {
+    var selectedLeague = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
@@ -17,8 +19,31 @@ class LeagueActivity : BaseActivity() {
 //            startActivity(skillIntent)
 //        }
     }
+    fun onMensClicked(view: View) {
+        womensbtn.isChecked = false
+        coedbtn.isChecked = false
+        selectedLeague = "men"
+    }
+    fun onWomensClicked(view: View) {
+        mensbtn.isChecked = false
+        coedbtn.isChecked = false
+        selectedLeague = "women"
+    }
+    fun onCoedClicked(view: View) {
+        mensbtn.isChecked = false
+        womensbtn.isChecked = false
+        selectedLeague = "coed"
+    }
     fun onNextBtnClick(view: View) { // another way of onclick action for buttons :: cool
-        val skillIntent = Intent(this, SkillselectionActivity::class.java)
-        startActivity(skillIntent)
+        if(!mensbtn.isChecked && !womensbtn.isChecked && !coedbtn.isChecked)
+            selectedLeague = ""
+        if(selectedLeague != "") {
+            val skillIntent = Intent(this, SkillselectionActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillIntent)
+        } else {
+            Toast.makeText(this, "Select League Type \uD83D\uDE14", Toast.LENGTH_SHORT).show()
+            selectedLeague = ""
+        }
     }
 }
--- app/src/main/java/com/example/swoosh/SkillselectionActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillselectionActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SkillselectionActivity : BaseActivity() {
+    var league = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skillselection)
+        league = intent.getStringExtra(EXTRA_LEAGUE).toString()
     }
 }
