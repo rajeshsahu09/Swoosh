8d526419c7b1bb635a4684d1f37626caeeb7170e
rajeshsahu09 <rajeshsahuise@gmail.com>
2020-08-25 18:52:21 +0530

    created skill selection activity

---
+++ app/src/main/java/com/example/swoosh/BaseActivity.kt
@@ -0,0 +1,43 @@
+package com.example.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+    val log = "LifeCycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(log, "${javaClass.simpleName} onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        Log.d(log, "${javaClass.simpleName} onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(log, "${javaClass.simpleName} onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(log, "${javaClass.simpleName} onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(log, "${javaClass.simpleName} onStop")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(log, "${javaClass.simpleName} onRestart")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(log, "${javaClass.simpleName} onDestory")
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,11 +1,24 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+//        nextbtn.setOnClickListener {
+//            val skillIntent = Intent(this, SkillselectionActivity::class.java)
+//            startActivity(skillIntent)
+//        }
+    }
+    fun onNextBtnClick(view: View) { // another way of onclick action for buttons :: cool
+        val skillIntent = Intent(this, SkillselectionActivity::class.java)
+        startActivity(skillIntent)
     }
 }
